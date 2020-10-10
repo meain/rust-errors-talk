ae49c1ff839aa14d83075efeb8e4ba987c7eecfa
Abin Simon <abinsimon10@gmail.com>
2020-10-10 19:23:57 +0530

    add mdx-deck theming stuff

---
+++ theme.js
@@ -0,0 +1,11 @@
+export default {
+  fonts: {
+    body: "Inconsolata", // Iosevka
+    monospace: "TerminessTTF NF",
+  },
+  colors: {
+    text: "black",
+    background: "white",
+    primary: "blue",
+  },
+};
